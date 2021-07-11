fbacf225cc1d139067c14230e51dc5aae754ce7d
ApurboStarry <hasinapurbo984@gmail.com>
2021-07-11 09:39:43 +0600

    prepared for deployment

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ require("./startup/cors")(app);
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
+require("./startup/prod")(app);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log("Listening on port", port));
